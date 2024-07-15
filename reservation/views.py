92a04f577e7e54fcc169d9f68e4b2110032d47c2
Tomasz Nilipiuk <tnil1143@gmail.com>
2024-07-15 02:06:14 +0000

    commit

--- reservation/views.py
+++ reservation/views.py
@@ -1,26 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
 from .models import Reservation
 from .forms import ReservationForm
-from django.contrib import messages
 
 # Create your views here.
 
 
 def reserve_table(request):
 
-    reservation_form = ReservationForm()
-
     if request.method == "POST":
-        reservation_form = ReservationForm(request.POST)
+        reservation_form = ReservationForm(data=request.POST)
 
         if reservation_form.is_valid():
             reservation_form.save()
-            '''
             messages.add_message(
-                request,
-                messages.SUCCESS,
-                "Booking succesful!",
-            )'''
+                request, messages.SUCCESS, "Booking approved and waiting approval!!"
+            )
+    reservation_form = ReservationForm()
+
     return render(
         request,
         "reservation.html",
@@ -28,3 +26,8 @@ def reserve_table(request):
             "reservation_form": reservation_form,
         },
     )
+
+
+class ReservationList(generic.ListView):
+    queryset = Reservation.objects.all()
+    template_name = "reservation_list.html"
