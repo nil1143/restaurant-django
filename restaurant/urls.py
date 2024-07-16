c7ab8f49dbf91608b5b98fb0ab7c71897e1be7ae
Tomasz Nilipiuk <tnil1143@gmail.com>
2024-07-16 12:27:38 +0000

    added reservation_list.html to URLs

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls'), name='home'),
+    # path('reservation/', include('reservation.urls'), name='reserve_table'),
     path('reservation/', include('reservation.urls'), name='reserve_table'),
+
 ]
