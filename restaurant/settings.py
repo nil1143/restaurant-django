c69ed9f3609ef7b6787f4284424085ba58bbc6e6
Tomasz Nilipiuk <tnil1143@gmail.com>
2024-07-20 03:01:10 +0000

    meals app added to settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reservation',
     'home',
+    'reservation',
+    'meals',
+    
 ]
 
 MIDDLEWARE = [
