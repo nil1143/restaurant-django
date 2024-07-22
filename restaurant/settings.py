28cfcb0001e30700f0a09d7d82b7b46659b7091a
Tomasz Nilipiuk <tnil1143@gmail.com>
2024-07-22 08:48:13 +0000

    debug to false

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-nil1143-restaurantdjang-cr4v0sm2mwe.ws.codeinstitute-ide.net','.herokuapp.com']
 
